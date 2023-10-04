2dcb0499c3b1118f1524eaf70d13bfa409641cb2
iamcoolalan <iamalan1996@gmail.com>
2023-10-04 19:51:21 +0800

    feat: add logout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
@@ -32,10 +33,18 @@ const StyledButton = styled.button`
 `;
 
 const Footer = ({ amount }) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    localStorage.removeItem('authToken')
+
+    navigate('/login')
+  }
+
   return (
     <StyledFooter>
       <p>剩餘項目數： {amount}</p>
-      <StyledButton>登出</StyledButton>
+      <StyledButton onClick={handleClick}>登出</StyledButton>
     </StyledFooter>
   );
 };
